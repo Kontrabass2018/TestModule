46acd668cf698e9b9485f3217500512c89a11cd9
sauves <leonardsauve@gmail.com>
2024-11-21 16:23:09 -0500

    updated Docs

--- src/TestModule.jl
+++ src/TestModule.jl
@@ -2,10 +2,16 @@ module TestModule
 
 export fit, infer
 
+
+"   fit(model, data, params)
+Fits the model to the data. 
+"
 function fit(model, data, params)
     return model
 end 
-
+"   infer(model, train_data, test_data, params)
+Infers new data with the pre-trained model.
+"
 function infer(model, train_data, test_data, params)
     return model
 end 
