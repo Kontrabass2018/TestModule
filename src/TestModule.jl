cc62417cde6ac07a0ce4fcf290a37f78da5b685e
sauves <leonardsauve@gmail.com>
2024-11-21 15:59:46 -0500

    added Project

---
+++ src/TestModule.jl
@@ -0,0 +1,13 @@
+module TestModule
+
+export fit, infer
+
+function fit(model, data, params)
+    return model
+end 
+
+function infer(model, train_data, test_data, params)
+    return model
+end 
+
+end
